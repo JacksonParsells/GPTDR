57749a3c4872263616c1834f809b11b33b019d44
JacksonParsells <john.parsells@tufts.edu>
2023-03-01 18:39:31 -0500

    committing before merging sam's code

--- t.py
+++ t.py
@@ -2,24 +2,58 @@
 twilio.py wraps the twilio API for GPTDR
 """
 
+from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request, redirect
 import os
 from twilio.rest import Client
 from dotenv import load_dotenv
 
-# Set environment variables for your credentials
-# Read more at http://twil.io/secure
-
+app = Flask(__name__)
 load_dotenv('.env')
 
+# init constants
+DEFAULTINTROMESSAGE = "Welcome to GPTDR! Thank you for providing initial \
+                       information for the diagnosis. To continue on, please \
+                       answer a few more questions."
+TESTSUBCRIBEDNUMBER = '+13025841779'
+GPTDRPHONENUMBER = '+18885143317'
+
+# init variables
 key = "TWILIO_AUTH_TOKEN"
 account_sid = "AC51cd81e721fd58f313cfcc3738677592"
 auth_token = os.getenv(key)
 client = Client(account_sid, auth_token)
 
-call = client.calls.create(
-    url="http://demo.twilio.com/docs/voice.xml",
-    to="+13025841779",
-    from_="+18885143317"
-)
 
-print(call.sid)
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message(DEFAULTINTROMESSAGE)
+
+    return str(resp)
+
+
+@app.route("/sms", methods=['GET', 'POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Determine the right reply for this message
+    if body == 'hello':
+        resp.message("Hi!")
+    elif body == 'bye':
+        resp.message("Goodbye")
+
+    return str(resp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -8,27 +8,28 @@ usr_input = input("Hello what are your symptoms? ")
 
 
 response = openai.Completion.create(
-model="text-davinci-003",
-prompt=usr_input + "in order to diagnose this problem, what followup \
+    model="text-davinci-003",
+    prompt=usr_input + "in order to diagnose this problem, what followup \
 questions do you have? Please format followup questions as a list.",
-temperature=1,
-max_tokens=859,
-top_p=1,
-frequency_penalty=0,
-presence_penalty=0
+    temperature=1,
+    max_tokens=859,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
 )
 
 print(response.choices[0].text)
 usr_input = input()
 
 response = openai.Completion.create(
-model="text-davinci-003",
-prompt= "Here are the answers \n" + usr_input + "What do you think the problem is?",
-temperature=1,
-max_tokens=859,
-top_p=1,
-frequency_penalty=0,
-presence_penalty=0
+    model="text-davinci-003",
+    prompt="Here are the answers \n" + usr_input +
+    "What do you think the problem is?",
+    temperature=1,
+    max_tokens=859,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
 )
 
 print(response.choices[0].text)
